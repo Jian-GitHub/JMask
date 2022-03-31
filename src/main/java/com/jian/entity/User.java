df54e4a559513a631801db459a0ad0086dd34e2e
祁剑 <E_QiJian@iCloud.com>
2022-03-31 22:25:10 +0800

    优化完善

--- src/main/java/com/jian/entity/User.java
+++ src/main/java/com/jian/entity/User.java
@@ -14,6 +14,6 @@ public class User {
     private String password;//密码
     private String createDate;//创建时间
     private String lastDate;//最后登录时间
-    private boolean isOnLine;//是否在线
-    private boolean isAvatar;//是否有头像
+    private boolean onLine;//是否在线
+    private boolean hasAvatar;//是否有头像
 }
